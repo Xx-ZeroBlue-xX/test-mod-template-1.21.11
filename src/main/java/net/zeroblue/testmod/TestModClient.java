64110a2770be038d2f973fb370f3a3f1e80988ad
Paul Leich <paul.leich2017@gmail.com>
2026-02-24 16:22:09 +0100

    Initial commit

---
+++ src/main/java/net/zeroblue/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.zeroblue.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
